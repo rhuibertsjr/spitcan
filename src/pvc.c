aec68a5b4204e8fd61aa529753b2b9e019dfe5c5
rhuibertsjr <rhuibertsjr@gmail.com>
2023-11-14 18:15:11 +0100

    Added: Initialization, devices and transmition for can-bus

--- src/pvc.c
+++ src/pvc.c
@@ -1,12 +1,95 @@
 #include "pvc.h"
 
-#include <stdio.h>
+//= rhjr: can-bus
 
-void app_main(void)
+esp_err_t
+comms_can_initialize (spi_host_device_t spi_host)
 {
-	printf("Starting app_main...\n");
+  ASSERT(spi_host < SPI_HOST_MAX, "Invalid SPI host");
+  LOG(INFO, TAG_SPI, "Initializing");
 
-    for(;;) {
+  esp_err_t result;
+  spi_bus_config_t spi_bus_config = {
+    .sclk_io_num = PVC_SPI_PIN_CLOCK,
+    .miso_io_num = PVC_SPI_PIN_MISO,
+    .mosi_io_num = PVC_SPI_PIN_MOSI
+  };
 
-    }
+  result =
+    spi_bus_initialize(spi_host, &spi_bus_config, SPI_DMA_CH_AUTO);
+
+  ASSERT(result == ESP_OK, "Unexpected result");
+  return result;
+}
+
+esp_err_t
+comms_can_add_device (spi_host_device_t spi_host, spi_device_handle_t *device)
+{
+  // rhjr: Esp32 allows a maximum of three devices (slaves).
+  ASSERT(spi_host < SPI_HOST_MAX, "Invalid SPI host");
+  ASSERT(PVC_SPI_PIN_CLOCK_SPEED < SPI_MASTER_FREQ_20M, 
+         "PVC_SPI_PIN_CLOCK_SPEED exceeds the allowed clock of MCP2515.");
+
+  esp_err_t result;
+  
+  // rhjr: c-style null initialization?
+  spi_device_interface_config_t spi_device_config = {0};  
+  spi_device_config.clock_speed_hz = SPI_CLK_SRC_DEFAULT;
+  spi_device_config.spics_io_num = PVC_SPI_PIN_CS0;
+  spi_device_config.mode = PVC_SPI_MODE;
+  spi_device_config.queue_size = 7;
+
+  result =
+    spi_bus_add_device(spi_host, &spi_device_config , device);
+
+  LOG(INFO, TAG_SPI, "  - Added one device.");
+
+  ASSERT(result == ESP_OK, "Unexpected result");
+  return result;
+}
+
+esp_err_t
+comms_can_transmit (
+  spi_device_handle_t device, const uint8_t *data, uint32_t length_in_bytes)
+{
+  ASSERT(length_in_bytes > 0, "expected at least a length of 1");
+
+  esp_err_t result;
+  spi_transaction_t transaction = {0};
+
+  transaction.length = length_in_bytes * 8; 
+  transaction.tx_buffer = data;
+  transaction.user = (void*) 1;
+
+  // rhjr: TODO add polling.
+  result =
+    spi_device_transmit(device, &transaction);
+
+  ASSERT(result == ESP_OK, "Unexpected result");
+  return result;
+}
+
+//= rhjr: application
+
+void
+app_main (void)
+{
+  LOG(INFO, TAG_PLATFORM, "Initializing");
+
+  //- rhjr: serial communication
+  spi_device_handle_t spi_device;
+  comms_can_initialize(PVC_SPI_PIN);
+
+  comms_can_add_device(PVC_SPI_PIN, &spi_device);
+
+  uint8_t data = 69;
+
+  comms_can_transmit(spi_device, &data, 1);
+
+  //- rhjr: application loop
+  for (;;)
+  {
+    
+  }
+
 }
--- src/pvc.h
+++ src/pvc.h
@@ -6,11 +6,44 @@
 #endif
 
 #include "driver/gpio.h"
+#include "driver/spi_master.h"
 
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
-//- rhjr: Helpers
+#include <stdio.h>
+#include <stdint.h>
+#include <stdarg.h>
+
+//- rhjr: Helper macros
+
+#define internal static 
+#define STATEMENT(x) do { x } while(0);
+
 #define SECONDS(seconds) (((seconds) * 1000) / portTICK_PERIOD_MS)
 
-#endif
+//- rhjr: can-bus
+// https://docs.espressif.com/projects/esp-idf/en/latest/esp32/api-reference/
+// peripherals/spi_master.html#gpio-matrix-and-io-mux
+
+#define PVC_SPI_PIN              SPI2_HOST /* rhjr: SPI1_HOST is reserved. */
+
+#define PVC_SPI_PIN_CLOCK        0x14 
+#define PVC_SPI_PIN_CLOCK_SPEED  SPI_MASTER_FREQ_8M 
+
+#define PVC_SPI_PIN_MISO         0x12 
+#define PVC_SPI_PIN_MOSI         0x13 
+#define PVC_SPI_PIN_CS0          0x15 /* rhjr: Only the first chip. */
+#define PVC_SPI_PIN_CS           0x22
+
+#define PVC_SPI_MODE             0x0
+
+esp_err_t comms_can_initialize (spi_host_device_t spi_host);
+esp_err_t comms_can_add_device (
+  spi_host_device_t spi_host, spi_device_handle_t *device);
+
+esp_err_t comms_can_transmit (
+  spi_device_handle_t device, const uint8_t *data, uint32_t length_in_bytes);
+
+#endif // PVC_CORE_H
+// pvc.h ends here.
