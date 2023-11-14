0ad89096aa8157dec7097061373db0c1f82b8b29
rhuibertsjr <rhuibertsjr@gmail.com>
2023-11-14 20:58:29 +0100

    Refactor: comms_spi -> spitcan

--- src/pvc.c
+++ src/pvc.c
@@ -44,16 +44,16 @@ _pvc_monitor_assert (
 //= rhjr: can-bus
 
 esp_err_t
-comms_can_initialize (spi_host_device_t spi_host)
+pvc_spitcan_initialize (spi_host_device_t spi_host)
 {
   ASSERT(spi_host < SPI_HOST_MAX, "Invalid SPI host");
-  LOG(INFO, TAG_SPI, "Initializing");
+  LOG(TAG_SPI, INFO, "Initializing");
 
   esp_err_t result;
   spi_bus_config_t spi_bus_config = {
-    .sclk_io_num = PVC_SPI_PIN_CLOCK,
-    .miso_io_num = PVC_SPI_PIN_MISO,
-    .mosi_io_num = PVC_SPI_PIN_MOSI
+    .sclk_io_num = PVC_SPI_PIN_SCK,
+    .mosi_io_num = PVC_SPI_PIN_SDO,
+    .miso_io_num = PVC_SPI_PIN_SDI
   };
 
   result =
@@ -64,33 +64,33 @@ comms_can_initialize (spi_host_device_t spi_host)
 }
 
 esp_err_t
-comms_can_add_device (spi_host_device_t spi_host, spi_device_handle_t *device)
+pvc_spitcan_add_device (spi_host_device_t spi_host, spi_device_handle_t *device)
 {
   // rhjr: Esp32 allows a maximum of three devices (slaves).
   ASSERT(spi_host < SPI_HOST_MAX, "Invalid SPI host");
-  ASSERT(PVC_SPI_PIN_CLOCK_SPEED < SPI_MASTER_FREQ_20M, 
+  ASSERT(PVC_SPI_SCK_FREQ < SPI_MASTER_FREQ_20M, 
          "PVC_SPI_PIN_CLOCK_SPEED exceeds the allowed clock of MCP2515.");
 
   esp_err_t result;
   
   // rhjr: c-style null initialization?
   spi_device_interface_config_t spi_device_config = {0};  
-  spi_device_config.clock_speed_hz = SPI_CLK_SRC_DEFAULT;
-  spi_device_config.spics_io_num = PVC_SPI_PIN_CS0;
-  spi_device_config.mode = PVC_SPI_MODE;
-  spi_device_config.queue_size = 7;
+  spi_device_config.clock_speed_hz = PVC_SPI_SCK_FREQ;
+  spi_device_config.spics_io_num   = PVC_SPI_PIN_CS0;
+  spi_device_config.mode           = PVC_SPI_MODE;
+  spi_device_config.queue_size     = PVC_SPI_QUEUE_LEN;
 
   result =
     spi_bus_add_device(spi_host, &spi_device_config , device);
 
-  LOG(INFO, TAG_SPI, "  - Added one device.");
+  LOG(TAG_SPI, INFO, "  - Added one device.");
 
   ASSERT(result == ESP_OK, "Unexpected result");
   return result;
 }
 
 esp_err_t
-comms_can_transmit (
+pvc_spitcan_transmit (
   spi_device_handle_t device, const uint8_t *data, uint32_t length_in_bytes)
 {
   ASSERT(length_in_bytes > 0, "expected at least a length of 1");
@@ -115,17 +115,17 @@ comms_can_transmit (
 void
 app_main (void)
 {
-  LOG(INFO, TAG_PLATFORM, "Initializing");
+  LOG(TAG_PLATFORM, INFO, "Initializing");
 
   //- rhjr: serial communication
   spi_device_handle_t spi_device;
-  comms_can_initialize(PVC_SPI_PIN);
+  pvc_spitcan_initialize(PVC_SPI_PIN);
 
-  comms_can_add_device(PVC_SPI_PIN, &spi_device);
+  pvc_spitcan_add_device(PVC_SPI_PIN, &spi_device);
 
-  uint8_t data = 69;
+  //uint8_t data = 69;
 
-  comms_can_transmit(spi_device, &data, 1);
+  //pvc_spitcan_transmit(spi_device, &data, 1);
 
   //- rhjr: application loop
   for (;;)
--- src/pvc.h
+++ src/pvc.h
@@ -1,10 +1,6 @@
 #ifndef PVC_CORE_H
 #define PVC_CORE_H
 
-#if !defined( __GNUC__ )
-    #error "Unsupported compiler, please use xtensa-esp32-elf-gcc."
-#endif
-
 #include "driver/gpio.h"
 #include "driver/spi_master.h"
 
@@ -15,42 +11,58 @@
 #include <stdint.h>
 #include <stdarg.h>
 
-//- rhjr: Helper macros
+//= rhjr: helpers
 
 #define internal static 
+
+//- rhjr: helper functions
+
 #define STATEMENT(x) do { x } while(0);
 
-#define SECONDS(seconds) (((seconds) * 1000) / portTICK_PERIOD_MS)
+//= rhjr: serial peripheral interface
 
-//- rhjr: can-bus
-// https://docs.espressif.com/projects/esp-idf/en/latest/esp32/api-reference/
-// peripherals/spi_master.html#gpio-matrix-and-io-mux
+#define PVC_SPI_MODE      0x0
+#define PVC_SPI_SCK_FREQ  SPI_MASTER_FREQ_8M 
+#define PVC_SPI_QUEUE_LEN 0x7
 
-#define PVC_SPI_PIN              SPI2_HOST /* rhjr: SPI1_HOST is reserved. */
+#if PVC_SPI_MASTER
+#  define PVC_SPI_PIN     SPI2_HOST /* rhjr: SPI1_HOST is reserved.           */
 
-#define PVC_SPI_PIN_CLOCK        0x14 
-#define PVC_SPI_PIN_CLOCK_SPEED  SPI_MASTER_FREQ_8M 
+#  define PVC_SPI_PIN_SCK 0x14 
+#  define PVC_SPI_PIN_SDO 0x13 /* MOSI                                        */
+#  define PVC_SPI_PIN_SDI 0x12 /* MISO                                        */
+#  define PVC_SPI_PIN_CS0 0x15
+#  define PVC_SPI_PIN_CSB 0x22
 
-#define PVC_SPI_PIN_MISO         0x12 
-#define PVC_SPI_PIN_MOSI         0x13 
-#define PVC_SPI_PIN_CS0          0x15 /* rhjr: Only the first chip. */
-#define PVC_SPI_PIN_CS           0x22
+#  define PVC_SPI_SLAVE   0x0
+#else // PVC_SPI_SLAVE
+#  define PVC_SPI_PIN     RCV_HOST 
 
-#define PVC_SPI_MODE             0x0
+#  define PVC_SPI_PIN_SCK 0x14 
+#  define PVC_SPI_PIN_SDO 0x13 /* MOSI                                        */
+#  define PVC_SPI_PIN_SDI 0x12 /* MISO                                        */
+#  define PVC_SPI_PIN_CS0 0x15 /* rhjr: Reserved for first slave.             */
+#  define PVC_SPI_PIN_CSB 0x22
 
-esp_err_t comms_can_initialize (spi_host_device_t spi_host);
-esp_err_t comms_can_add_device (
+#  define PVC_SPI_MASTER  0x0
+#  define PVC_SPI_SLAVE   0x1
+#endif  
+
+//- rhjr: api
+
+esp_err_t pvc_spitcan_initialize (spi_host_device_t spi_host);
+esp_err_t pvc_spitcan_add_device (
   spi_host_device_t spi_host, spi_device_handle_t *device);
 
-esp_err_t comms_can_transmit (
+esp_err_t pvc_spitcan_transmit (
   spi_device_handle_t device, const uint8_t *data, uint32_t length_in_bytes);
 
-//= rhjr: pvc monitoring
+//= rhjr: abort, assertions & logging
 
 typedef enum pvc_monitor_type pvc_monitor_type;
 enum pvc_monitor_type
 {
-  ERROR = 0x00, // rhjr: always expect error
+  ERROR         = 0x00, 
   WARNING,
   INFO
 };
@@ -58,48 +70,53 @@ enum pvc_monitor_type
 typedef enum pvc_monitor_tag pvc_monitor_tag;
 enum pvc_monitor_tag
 {
-  TAG_NONE = 0x00,
+  TAG_NONE      = 0x00,
   TAG_PLATFORM,
   TAG_ASSERT,
-  TAG_SPI,
-  TAG_MAX
+  TAG_SPI
 };
 
-const char *_pvc_monitor_type_table[] = {
-  "ERROR", "WARNING", "INFO"};
+// rhjr: lookup tables
+const char *_pvc_monitor_type_table[] =
+{
+  [ERROR]   = "ERROR",
+  [WARNING] = "WARNING",
+  [INFO]    = "INFO"
+};
 
-const char *_pvc_monitor_tag_table[] = {
+const char *_pvc_monitor_tag_table[] =
+{
   [TAG_NONE]     = "DEBUG",
   [TAG_PLATFORM] = "Platform",
   [TAG_ASSERT]   = "ASSERTION",
   [TAG_SPI]      = "SPI"
 };
 
+//- rhjr: logging
+
 internal void
 _pvc_monitor_stdout_log(
   pvc_monitor_tag tag, pvc_monitor_type type, const char* format, ...);
+
+#if PVC_LOGGING
+#  define LOG(tag, type, message, ...)                                        \
+      STATEMENT(_pvc_monitor_stdout_log(tag, type, message, ##__VA_ARGS__);)
+#else
+#  define LOG(tag, type, message, ...)
+#endif
+
+//- rhjr: abort & assertions
 
 internal _Noreturn uintptr_t
 _pvc_monitor_assert (
   const char* condition, const char* file, uint32_t line, const char *msg, ...);
 
-//- rhjr: pvc logging
-
-#ifdef PVC_LOGGING
-# define LOG(tag, type, message, ...)                                          \
-  STATEMENT(_pvc_monitor_stdout_log(tag, type, message, ##__VA_ARGS__);)
-#else
-# define LOG(tag, type, message, ...)
-#endif
-
-//- rhjr: pvc assert
-
-#ifdef PVC_ASSERT
-# define ASSERT(condition, msg, ...)                                           \
+#if PVC_ASSERT
+#  define ASSERT(condition, msg, ...)                                          \
     STATEMENT( if(!(condition)) {                                              \
       _pvc_monitor_assert(#condition, __FILE__, __LINE__, msg, ##__VA_ARGS__);})
 #else
-# define ASSERT(condition, msg, ...)
+#  define ASSERT(condition, msg, ...)
 #endif
 
 #endif // PVC_CORE_H
